b75225bfa4531553d3f13a02b9cc72d0b13c7dc5
Maladra <Julien.ehles@hotmail.fr>
2019-11-25 15:29:04 +0100

    first commit

---
+++ citation/citation/spiders/citation_spider.py
@@ -0,0 +1,17 @@
+import scrapy
+
+class CitationSpider(scrapy.Spider):
+    name="citation"
+    start_urls = ['http://www.1001-citations.com/page/1/',]
+
+    def parse(self, response):
+        for quote in response.css('div'):
+            yield {
+                'text' : quote.css('h2 ::text').get(),
+                'author' : quote.css('span.author ::text').getall(),
+            }
+
+        next_page = response.css('a.nextpostslink::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
