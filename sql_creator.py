ce373e2da3d33883753366c8ff9938ca61fe99a4
Maladra <julien.ehles@hotmail.fr>
2019-11-27 00:17:03 +0100

    add sql file

--- sql_creator.py
+++ sql_creator.py
@@ -1,4 +1,5 @@
 import json
+import sqlite3
 
 class Citation:
     def __init__(self,text,author,tags):
@@ -35,25 +36,29 @@ class Tag:
 #################
 ## STAT by Tag ##
 #################
-tag_array = []
-i = 0
-add = True
-with open('citation/citation.json') as json_file:
-    data = json.load(json_file)
-    for p in data:
-        for tag in p['tags']:
-            for item in tag_array:
-                if tag == item.tag:
-                    item.counter = item.counter+1
-                    add = False
-                    break
-            if add : 
-                tag = Tag(p['tags'])
-                tag_array.append(tag)
-            add = True
-            print (i)
-            i = i+1
+#tag_array = []
+#i = 0
+#add = True
+#with open('citation/citation.json') as json_file:
+#    data = json.load(json_file)
+#    for p in data:
+#        for tag in p['tags']:
+#            for item in tag_array:
+#                if tag == item.tag:
+#                    item.counter = item.counter+1
+#                    add = False
+#                    break
+#            if add : 
+#                tag = Tag(p['tags'])
+#                tag_array.append(tag)
+#            add = True
+#            print (i)
+#            i = i+1
+#
+#
+#print (len(tag_array))
 
 
-print (len(tag_array))
+conn = sqlite3.connect('citation.db')
+c = conn.cursor()
 
