cfd6cb83060b1ffbff897e1a206cc925466e74f0
Maladra <Julien.ehles@hotmail.fr>
2019-11-26 08:47:43 +0100

    iteration correcte

--- stat_creator.py
+++ stat_creator.py
@@ -4,8 +4,6 @@ with open('citation/citation.json') as json_file:
     data = json.load(json_file)
     for p in data:
         print(p['text'])
-        for author in p['author']:
-            if author != '- ':
-                print(author)
-
-
+        print(p['author'])
+        for tag in p['tags']:
+            print(tag)
